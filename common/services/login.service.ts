244376a6e85d40bd2dd95bede601676056465acd
brunaalvesws <baws@cin.ufpe.br>
2022-05-04 16:49:28 -0300

    criação das funções de login

--- common/services/login.service.ts
+++ common/services/login.service.ts
@@ -1,36 +1,42 @@
 import { Injectable } from '@angular/core';
 import { Aluno } from '../classes/aluno';
-
 import { Professor } from '../classes/professor';
+import { CadastroDeAlunos } from '../../ta-server/cadastrodealunos';
+import { CadastroDeProfessor } from '../../ta-server/cadastrodeprofs';
 
 @Injectable()
 export class LoginService {
-  type: string = '';
-  account: Professor | Aluno | null = null;
+  constructor(private cadastroAlunos: CadastroDeAlunos, private cadastroProf: CadastroDeProfessor) {}
 
-  login(account: Professor | Aluno, type: string) {
-    this.account = account;
-    this.type = type;
+  loginAluno (cpf: string, senha: string) : string{
+    if (this.cadastroAlunos.getAlunos().find(a => a.cpf == cpf)) {
+      if (this.cadastroAlunos.getAlunos().find(a => a.cpf == cpf && a.senha == senha)){
+        return "success"
+      } else {
+        return "passworderror"
+      }
+    } else {
+      return "cpferror"
+    }
   }
 
-  getType(): string {
-    return this.type;
+  loginProf (cpf: string, senha: string) : string {
+    if (this.cadastroProf.getProfessores().find(a => a.cpf == cpf) && this.checkCPF(cpf)) {
+      if (this.cadastroProf.getProfessores().find(a => a.cpf == cpf && a.senha == senha)){
+        return "success"
+      } else {
+        return "passworderror"
+      }
+    } else {
+      return "cpferror"
+    }
   }
 
-  getAccount(): Professor | Aluno | null {
-    return this.account;
+  checkCPF(cpf: string) : boolean {
+    if (/^[0-9]+$/.test(cpf)) {
+      return true
+    } else {
+      return false
+    }
   }
-
-  
 }
-
-cpfNaoCadastrado(cpf: string): boolean {
-  return !this.alunos.find(a => a.cpf == cpf);
-}
-
-checksenha(cpf: string, senha: string): boolean {
- return !this.alunos.find(a => a.cpf == cpf && a.senha == senha);
-}
-
-getAlunoCPFPass(cpf: string, senha: string): Aluno {
- return this.alunos.find(a => a.cpf == cpf && a.senha == senha);
