23afcb464b239a808540f98228fe1b867b487290
brunaalvesws <baws@cin.ufpe.br>
2022-05-05 16:22:05 -0300

    inicio do auth

--- ta-server/auth.ts
+++ ta-server/auth.ts
@@ -1,37 +1,21 @@
-import { LoginService } from '../common/services/login.service';
-import { getAuth, signInWithCustomToken } from "firebase/auth";
+import { LoginService } from './login.service';
+import { getAuth, signInWithCustomToken, signOut} from "firebase/auth";
+import { initializeApp } from "firebase/app";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCnbHVORHVXbB6jwlzFdOYoYcZxBKMoNOs",
-    authDomain: "siga-ess.firebaseapp.com",
-    projectId: "siga-ess",
-    storageBucket: "siga-ess.appspot.com",
-    messagingSenderId: "472548883315",
-    appId: "1:472548883315:web:e2ffddf7d16d9ab72c2306",
-    measurementId: "G-8WY0WPWXYD"
-  };
-
-var firebaseAdmin = require('firebase-admin');
-
-firebaseAdmin.initializeApp({
-credential: firebaseAdmin.credential.cert('cert.json'),
-databaseURL: 'https://siga-ess.firebaseio.com'
-});
-
-var firebaseAdmin = require('firebase-admin');
-var authenticate = new LoginService();
-
-@Injectable()
-export class Auth {
+export class FBAuth implements OnInit{
+    var admin;
+    var firebaseConfig;
+    var app;
+    var serviceAccount;
 
     criarToken(cpf: string): string {
-        firebaseAdmin.auth().createCustomToken(cpf).then((token) => {
+        admin.auth().createCustomToken(cpf).then((token) => {
             return token;
         })
         .catch(error) {
             return error.message;
         });
-    }
+    };
 
     authLogin(token: string): void {
         const auth = getAuth();
@@ -43,5 +27,30 @@ export class Auth {
         const errorCode = error.code;
         const errorMessage = error.message;
         });
-}
-module.exports = router;
+    };
+
+    authLogout(): void {
+        const auth = getAuth();
+        auth.signOut();
+    }
+
+      
+
+    OnInit {
+        this.firebaseConfig = {
+            apiKey: "AIzaSyCnbHVORHVXbB6jwlzFdOYoYcZxBKMoNOs",
+            authDomain: "siga-ess.firebaseapp.com",
+            projectId: "siga-ess",
+            storageBucket: "siga-ess.appspot.com",
+            messagingSenderId: "472548883315",
+            appId: "1:472548883315:web:e2ffddf7d16d9ab72c2306",
+            measurementId: "G-8WY0WPWXYD"
+        };
+        this.app = initializeApp(this.firebaseConfig);
+        this.admin = require("firebase-admin");
+        this.serviceAccount = require("./cert.json");
+        this.admin.initializeApp({
+            credential: this.admin.credential.cert(this.serviceAccount)
+        });
+    }
+};
