62537a1e18f11270d4621c9f9ed491c2559c72b1
brunaalvesws <baws@cin.ufpe.br>
2022-05-11 00:25:25 -0300

    testes de matricula do aluno na cadeira

---
+++ tests-acceptance/features/matricula.feature
@@ -0,0 +1,23 @@
+Feature: Teste matricula
+
+Scenario: Matricula em uma cadeira
+Given Estou na página de autenticação de usuários
+Given Criei um usuário com o email "fgm4@cin.com", o nome "Filipe Melo", a senha "filipe12345" e escolhi o tipo Professor
+When Tento fazer login com email "fgm4@cin.com" e senha "filipe12345"
+Then Vou para a tela inicial do usuário
+Then Estou logado como Professor
+When Clico no botão de adicionar nova cadeira
+When Adiciono o nome da disciplina "Introdução a Programação", o departamento "CIn", o total de vagas "60" e marco os horários "seg8, seg9, qua10, qua11"
+When Clico no botão de finalizar nova cadeira
+Then Vejo uma mensagem de confirmação do cadastro da cadeira
+Then Vou para a tela inicial do usuário
+When Clico no botão de LogOut
+Then Estou na página de autenticação de usuários
+Given Criei um usuário com o email "baws1@cin.com", o nome "Bruna", a senha "bruna12345" e escolhi o tipo Aluno
+When Tento fazer login com email "baws1@cin.com" e senha "bruna12345"
+Then Vou para a tela inicial do usuário
+Then Estou logado como Aluno
+When Seleciono o departamento "CIn" e a cadeira "Introdução a Programação"
+When Clico no botão Selecionar
+When Clico no botão Matricular 
+Then Vejo uma mensagem de confirmação da matrícula
---
+++ tests-acceptance/stepdefinitions/matricula.ts
@@ -0,0 +1,32 @@
+const { Given, When, Then, And } = require("@cucumber/cucumber");
+const { browser, $, element, ElementArrayFinder, by, driver, ExpectedConditions } = require('protractor');
+
+import { Alert } from 'selenium-webdriver';
+let chai = require('chai').use(require('chai-as-promised'));
+let expect = chai.expect;
+
+When(/^Clico no botão de LogOut$/, async () => {
+    await $("a[name='logout']").click();
+});
+
+When(/^Seleciono o departamento "([^\"]*)" e a cadeira "([^\"]*)$/, async (departamento, nome) => {
+    await element(by.tagName("select#selectDepartamento")).click();
+    await element(by.cssContainingText('option', departamento)).click();
+    await element(by.tagName("select#selectCadeira")).click();
+    await element(by.cssContainingText('option',nome)).click();
+})
+
+When(/^Clico no botão Selecionar$/, async () => {
+    await element(by.buttonText('Selecionar')).click();
+});
+
+When(/^Clico no botão Matricular $/, async () => {
+    await element(by.buttonText('Matricular')).click();
+});
+
+Then(/^Vejo uma mensagem de confirmação da matrícula$/, async () => {
+    await browser.wait(ExpectedConditions.alertIsPresent(), 1000000); 
+    let ale = await browser.switchTo().alert()
+    await expect(ale.getText()).to.eventually.equal('Matricula realizada com sucesso');
+    await ale.accept();
+});
